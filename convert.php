4dd77960e56f6486e9950f63ff07a917bb3ec89c
EzioisAwesome56 <jedijon567@gmail.com>
2017-03-16 22:33:31 -0400

    still trying to fix that same bug

--- convert.php
+++ convert.php
@@ -127,6 +127,8 @@ if ($_POST['submit'])
 	Upgrade();
 	Import('database.sql');
 	
+	Query("insert into {users} (id, name, password, pss, primarygroup, regdate, lastactivity, lastip, email, sex, theme) values ({0}, {1}, {2}, {3}, {4}, {5}, {5}, {6}, {7}, {8}, {9})", 
+		-1, 'banned', 'dfsafs', 'fsdaf', -1, time(), 'dankweed', '', 2, 'abxd3');	
 	//here, instead of trying to detect a user's rank, just change the coloum name. way easier
 	Query("ALTER TABLE users CHANGE `powerlevel` `primarygroup` int(11)")
 		
